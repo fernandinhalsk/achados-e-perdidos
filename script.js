f2be742fcaec78ed489fea479184ca8e4392b13c
fernandinhalsk <fernanda.climaco@escola.pr.gov.br>
2025-10-29 10:29:41 -0300

    dois

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
 const itens = [
   {
+    tipo: "mochila",
+    cor: "rosa",
+    local: "sala 203",
+    data: "25/10",
     nome: "Mochila Rosa",
     descricao: "Encontrada na sala 203 em 25/10",
     imagem: "imagens/mochila-rosa.jpg"
   },
   {
+    tipo: "estojo",
+    cor: "rosa",
+    local: "biblioteca",
+    data: "27/10",
     nome: "Estojo com glitter",
     descricao: "Encontrado na biblioteca em 27/10",
     imagem: "imagens/estojo-glitter.jpg"
   }
 ];
 
-function buscarItens() {
+function filtrarItens() {
+  const tipo = document.getElementById("tipoFiltro").value;
+  const cor = document.getElementById("corFiltro").value;
+  const local = document.getElementById("localFiltro").value;
+  const data = document.getElementById("dataFiltro").value;
   const container = document.getElementById("itensContainer");
-  const termo = document.getElementById("searchInput").value.toLowerCase();
   container.innerHTML = "";
 
   const filtrados = itens.filter(item =>
-    item.nome.toLowerCase().includes(termo) ||
-    item.descricao.toLowerCase().includes(termo)
+    (tipo === "" || item.tipo === tipo) &&
+    (cor === "" || item.cor === cor) &&
+    (local === "" || item.local === local) &&
+    (data === "" || item.data === data)
   );
 
   filtrados.forEach(item => {
@@ -34,6 +47,6 @@ function buscarItens() {
   });
 
   if (filtrados.length === 0) {
-    container.innerHTML = "<p>Nenhum item encontrado.</p>";
+    container.innerHTML = "<p>Nenhum item encontrado com esses filtros.</p>";
   }
 }
